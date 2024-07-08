a4e140a9d73e1ba2844e7a6c1229bf9ac8d1a26c
kevin lima <kevin.lima@cwi.com.br>
2024-07-08 02:56:36 -0300

    adicionando melhorias visuais

--- main.py
+++ main.py
@@ -2,23 +2,66 @@ import os
 import wave_analise
 import wave_processor
 from datetime import datetime
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk
+from tkinter import PhotoImage
+
+root = tk.Tk()
+
+def read_wav_file():
+    # Open file dialog and get the path of the selected file
+    file_path = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
+    if not file_path:
+        print("No file selected.")
+        return
+
+    return file_path
+
+def obterFormula(nome_arquivo, identificador):
+    with open(nome_arquivo, 'r') as arquivo:
+        for linha in arquivo:
+            if linha.startswith(identificador + "="):
+                return linha.split("=", 1)[1].strip()
+    return None
+
 def main():
-        directory = './input'
-        if os.path.exists(directory):
-        # Iterate over all files in the directory
-                for archive in os.listdir(directory):
-                        filepath = os.path.join(directory, archive)
-                        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-                        #apply dft
-                        #
-                        buffer = wave_analise.save_audio_in_chunks(filepath, chunk_size_ms=100)
-                        processed_waves = wave_processor.process(buffer)
-                        
-                        # Define filename with timestamp
-                        filename = f"output_{timestamp}_{archive}.txt"
-                        print(processed_waves)
-                        # Save output to file
-                        with open('./output/' + filename, 'w') as f:
-                                f.write(str(processed_waves))
-        return "OK"
-main()
+        filepath = read_wav_file()
+        if not filepath:
+                return 
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        root.title("Data Table")
+        root.config(padx=10, pady=100)
+        #apply dft
+        buffer = wave_analise.save_audio_in_chunks(filepath, chunk_size_ms=100)
+        processed_waves = wave_processor.process(buffer)
+        
+        # Define filename with timestamp
+        filename = f"output_{timestamp}_processed.txt"
+        print(processed_waves)
+        # Save output to file
+        with open('./output/' + filename, 'w') as f:
+                f.write(str(processed_waves))
+        # The data to be displayed
+
+                # Create the treeview
+        tree = ttk.Treeview(root, columns=("analised", "formula", "percent"), show="headings")
+        tree.heading("analised", text="Analised")
+        tree.heading("formula", text="Formula")
+        tree.heading("percent", text="Percent")
+
+        # Add data to the treeview
+        for item in processed_waves:
+            nome_sem_extensao, _ = os.path.splitext(item["simility"]["formula"])
+            formula = obterFormula(nome_arquivo="./generateFormula/function.txt", identificador=nome_sem_extensao)
+            tree.insert("", tk.END, values=(item["analised"], formula, item["simility"]["percent"]))
+
+        # Pack the treeview into the window
+        tree.pack(expand=True, fill=tk.BOTH)
+        # Start the GUI event loop
+        root.mainloop()
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ neural.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+import numpy as np
+
+class Neural(object):
+
+	""" Implementation of VGG16 network """
+
+	def __init__(self, x, keep_prob, num_classes):
+
+		"""Create the graph of the AlexNet model.
+
+        Args:
+            x: Placeholder for the input tensor.
+            keep_prob: Dropout probability.
+            num_classes: Number of classes in the dataset.
+        """
+
+        # Parse input arguments into class variables
+		self.X = x
+		self.NUM_CLASSES = num_classes
+		self.KEEP_PROB = keep_prob
+
+		# Call the create function to build the computational graph of AlexNet
+		self.create()
+
+	def create(self):
+
+		"""Create the network graph."""
+
+		conv1_1 = conv_layer(self.X, 64, 'conv1_1')
+		conv1_2 = conv_layer(conv1_1, 64, 'conv1_2')
+		pool1 = max_pool(conv1_2, 'pool1')
+
+		conv2_1 = conv_layer(pool1, 128, 'conv2_1')
+		conv2_2 = conv_layer(conv2_1, 128, 'conv2_2')
+		pool2 = max_pool(conv2_2, 'pool2')
+
+		conv3_1 = conv_layer(pool2, 256, 'conv3_1')
+		conv3_2 = conv_layer(conv3_1, 256, 'conv3_2')
+		conv3_3 = conv_layer(conv3_2, 256, 'conv3_3')
+		pool3 = max_pool(conv3_3, 'pool3')
+
+		conv4_1 = conv_layer(pool3, 512, 'conv4_1')
+		conv4_2 = conv_layer(conv4_1, 512, 'conv4_2')
+		conv4_3 = conv_layer(conv4_2, 512, 'conv4_3')
+		pool4 = max_pool(conv4_3, 'pool4')
+
+		conv5_1 = conv_layer(pool4, 512, 'conv5_1')
+		conv5_2 = conv_layer(conv5_1, 512, 'conv5_2')
+		conv5_3 = conv_layer(conv5_2, 512, 'conv5_3')
+		pool5 = max_pool(conv5_3, 'pool5')
+
+		flattened = tf.reshape(pool5, [-1, 1 * 1 * 512])
+		fc6 = fc_layer(flattened, 1 * 1 * 512, 4096, name = 'fc6')
+		dropout6 = dropout(fc6, self.KEEP_PROB)
+
+		fc7 = fc_layer(dropout6, 4096, 4096, name = 'fc7')
+		dropout7 = dropout(fc7, self.KEEP_PROB)
+
+		self.fc8 = fc_layer(dropout7, 4096, self.NUM_CLASSES, relu = False, name = 'fc8')
+
+
+
+def conv_layer(x, num_filters, name, filter_height = 3, filter_width = 3,
+	stride = 1, padding = 'SAME'):
+
+	"""Create a convolution layer."""
+	
+	# Get number of input channels
+	input_channels = int(x.get_shape()[-1])
+
+	with tf.variable_scope(name) as scope:
+
+		# Create tf variables for the weights and biases of the conv layer
+		W = tf.get_variable('weights', shape = [filter_height, filter_width, input_channels, num_filters],
+			initializer = tf.random_normal_initializer(mean = 0.0, stddev = 0.01))
+
+		b = tf.get_variable('biases', shape = [num_filters], initializer = tf.constant_initializer(0.0))
+
+		# Perform convolution.
+		conv = tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = padding)
+		# Add the biases.
+		z = tf.nn.bias_add(conv, b)
+		# Apply ReLu non linearity.
+		a = tf.nn.relu(z)
+
+		return a
+
+def fc_layer(x, input_size, output_size, name, relu = True):
+
+	"""Create a fully connected layer."""
+	
+	with tf.variable_scope(name) as scope:
+
+		# Create tf variables for the weights and biases.
+		W = tf.get_variable('weights', shape = [input_size, output_size],
+			initializer = tf.random_normal_initializer(mean = 0.0, stddev = 0.01))
+
+		b = tf.get_variable('biases', shape = [output_size], initializer = tf.constant_initializer(0.0))
+
+		# Matrix multiply weights and inputs and add biases.
+		z = tf.nn.bias_add(tf.matmul(x, W), b)
+
+		if relu:
+			# Apply ReLu non linearity.
+			a = tf.nn.relu(z)
+			return a
+
+		else:
+			return z
+
+def max_pool(x, name, filter_height = 2, filter_width = 2,
+	stride = 2, padding = 'VALID'):
+
+	"""Create a max pooling layer."""
+
+	return tf.nn.max_pool(x, ksize = [1, filter_height, filter_width, 1],
+		strides = [1, stride, stride, 1], padding = padding, name = name)
+
+def dropout(x, keep_prob):
+
+	"""Create a dropout layer."""
+
+	return tf.nn.dropout(x, keep_prob = keep_prob)
